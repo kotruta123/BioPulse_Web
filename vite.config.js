7439a3fdecf4c7a259f5c989dc1ad5177b29d0de
kotruta123 <95081082+kotruta123@users.noreply.github.com>
2024-12-09 11:58:46 +0100

    fix: fixed github pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/BioPulse_Web/',
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
