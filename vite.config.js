319a1cf4c0940de8b985638e75e480774b49aba5
kotruta123 <95081082+kotruta123@users.noreply.github.com>
2024-12-09 10:48:36 +0100

    fix: vite fix

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/BioPulse_Web', // Adjust your base path for deployment if necessary
+  base: '/BioPulse_Web/', // Adjust your base path for deployment if necessary
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'], // Include extensions
   },
   server: {
+    headers: {
+      'Content-Type': 'application/javascript',
+    },
     fs: {
       allow: ['..'],
     },
